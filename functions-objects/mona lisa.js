cbdf2d5d2bcac571db37f1fa49ea7871164be9e9
knayad <68199213+knayad@users.noreply.github.com>
2021-07-29 19:14:07 -0400

    Add files via upload

---
+++ functions-objects/mona lisa.js
@@ -0,0 +1,14 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+
+data
+
+var i = 0
+
+while (i < 4294) {
+    ball = data[i]
+    create(ball.x, ball.y, ball.color);
+    i++;
+}
